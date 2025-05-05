212fb0e396ff49eada42ff61479241f97479725b
boudy <bodynafea2@gmail.com>
2025-05-05 10:24:46 +0300

    setup project

---
+++ printo/src/main/java/com/team/printo/PrintoApplication.java
@@ -0,0 +1,13 @@
+package com.team.printo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintoApplication.class, args);
+	}
+
+}
---
+++ printo/src/test/java/com/team/printo/PrintoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.printo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
