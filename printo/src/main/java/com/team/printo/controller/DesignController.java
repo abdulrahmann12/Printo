8a0d7d62d26dde351c246acad6e4cefefcbafa14
boudy <bodynafea2@gmail.com>
2025-05-16 00:44:52 +0300

    create design module

---
+++ printo/src/main/java/com/team/printo/controller/DesignController.java
@@ -0,0 +1,63 @@
+package com.team.printo.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.team.printo.dto.BasicResponse;
+import com.team.printo.dto.DesignDTO;
+import com.team.printo.model.User;
+import com.team.printo.service.DesignService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/design")
+@RequiredArgsConstructor
+public class DesignController {
+
+	private final DesignService designService;
+	
+    @PostMapping("/product/{productId}")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<BasicResponse> addDesign(
+            @AuthenticationPrincipal UserDetails userDetails,
+            @PathVariable Long productId,
+            @Valid @RequestPart(value = "image", required = false) MultipartFile image
+    ) throws Exception {
+        Long userId = ((User) userDetails).getId();
+        DesignDTO newDesign = designService.addDesign(userId, productId, image);
+        return ResponseEntity.ok(new BasicResponse("Design uploaded successfully", newDesign));
+    }
+    
+    @GetMapping("/my-designs")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<List<DesignDTO>> getMyDesigns(
+            @AuthenticationPrincipal UserDetails userDetails
+    ) {
+        Long userId = ((User) userDetails).getId();
+        List<DesignDTO> designs = designService.getDesignsByUserId(userId);
+        return ResponseEntity.ok(designs);
+    }
+    
+    @DeleteMapping("/{designId}")
+	@PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<BasicResponse> deleteDesign(
+            @PathVariable Long designId
+    ) {
+        designService.deleteDesign(designId);
+        return ResponseEntity.ok(new BasicResponse("Design deleted successfully"));
+    }
+}
---
+++ printo/src/main/java/com/team/printo/service/DesignService.java
@@ -0,0 +1,73 @@
+package com.team.printo.service;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.team.printo.dto.DesignDTO;
+import com.team.printo.exception.ResourceNotFoundException;
+import com.team.printo.mapper.DesignMapper;
+import com.team.printo.model.Design;
+import com.team.printo.model.Product;
+import com.team.printo.model.User;
+import com.team.printo.repository.DesignRepository;
+import com.team.printo.repository.ProductRepository;
+import com.team.printo.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class DesignService {
+	private final UserRepository userRepository;
+	private final ProductRepository productRepository;
+	private final DesignRepository designRepository;
+	private final DesignMapper designMapper;
+	private final ImageService imageService;
+	
+	public DesignDTO addDesign(Long userId, Long productId ,MultipartFile image) throws Exception{
+	    Product product = productRepository.findById(productId)
+	            .orElseThrow(() -> new ResourceNotFoundException("Product not found"));
+	    User user = userRepository.findById(userId)
+	            .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+	    
+	    Design newDesign = new Design();
+	    
+	    if (image != null && !image.isEmpty()) {
+	        try {
+	            String imageUrl = imageService.uploadImage(image);
+	            newDesign.setImage(imageUrl);
+	        } catch (IOException e) {
+	            throw new Exception("Error occurred while saving image: " + e.getMessage());
+	        }
+	    }else if (image == null || image.isEmpty()) {
+	        throw new IllegalArgumentException("Image file is required.");
+	    }
+	    
+	    newDesign.setProduct(product);
+	    newDesign.setUser(user);
+	    
+	    Design savedDesign = designRepository.save(newDesign);
+	    
+	    return designMapper.toDto(savedDesign);
+	}
+	
+	public List<DesignDTO> getDesignsByUserId(Long userId) {
+	    User user = userRepository.findById(userId)
+	            .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+	    List<Design> designs = designRepository.findByUserId(user.getId());
+	    return designs.stream()
+	            .map(designMapper::toDto)
+	            .collect(Collectors.toList());
+	}
+	
+	public void deleteDesign(Long designId) {
+	    Design design = designRepository.findById(designId)
+	            .orElseThrow(() -> new ResourceNotFoundException("Design not found"));
+	    designRepository.delete(design);
+	}
+	
+}
