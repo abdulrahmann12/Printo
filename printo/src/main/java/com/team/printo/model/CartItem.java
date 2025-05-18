228103afbf27583a592c267b2ff7c93a5df44728
boudy <bodynafea2@gmail.com>
2025-05-18 18:46:33 +0300

    change relation between design and cart item

--- printo/src/main/java/com/team/printo/model/CartItem.java
+++ printo/src/main/java/com/team/printo/model/CartItem.java
@@ -11,7 +11,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -39,7 +38,7 @@ public class CartItem {
 	@JoinColumn(name = "cart_id", nullable = false)
 	private Cart cart;
 	
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "design_id")
     private Design design;
     
--- printo/src/main/java/com/team/printo/model/Design.java
+++ printo/src/main/java/com/team/printo/model/Design.java
@@ -1,11 +1,16 @@
 package com.team.printo.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -31,4 +36,7 @@ public class Design {
     @ManyToOne
     @JoinColumn(name = "product_id")
     private Product product;
+    
+    @OneToMany(mappedBy = "design", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> cartItems = new ArrayList<>();
 }
